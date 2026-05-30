8dff26a8ccf1d32092b0b8f9096748fb65307c32
Antoine <a.bourgeolet@gmail.com>
2026-05-30 07:59:15 +0200

    ci: update TWA manifest for GitHub Pages and regenerate build configuration

---
+++ TWA/app/src/main/java/app/run/europe_west1/it_s_semi_120888622943/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.run.europe_west1.it_s_semi_120888622943.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
